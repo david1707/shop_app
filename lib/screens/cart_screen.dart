b8eeaed606a751b9b7a5e223d44380a2c213b090
david1707 <DavidAlacant@gmail.com>
2019-08-09 19:55:11 +0200

    Added remove from cart functionality

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -47,7 +47,13 @@ class CartScreen extends StatelessWidget {
           Expanded(
             child: ListView.builder(
               itemCount: cart.items.length,
-              itemBuilder: (ctx, i) => CartItem(cart.items.values.toList()[i].id, cart.items.values.toList()[i].price, cart.items.values.toList()[i].quantity, cart.items.values.toList()[i].title),
+              itemBuilder: (ctx, i) => CartItem(
+                cart.items.values.toList()[i].id,
+                cart.items.keys.toList()[i],
+                cart.items.values.toList()[i].price,
+                cart.items.values.toList()[i].quantity,
+                cart.items.values.toList()[i].title,
+              ),
             ),
           )
         ],
